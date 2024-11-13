8fd9db1e11d869d43921e65ff045966ee901003b
Med Rami Brick <brickrami1@gmail.com>
2024-11-13 13:22:52 +0100

    'clear'

--- src/main/java/com/capfest/event/repository/evenementRepo.java
+++
@@ -1,9 +0,0 @@
-package com.capfest.event.repository;
-
-import com.capfest.event.entite.evenement;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface evenementRepo extends JpaRepository<evenement, Long> {
-}
